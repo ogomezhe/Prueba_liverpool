4b037bd4780af33e6a9bda12110a22d2c5f8b479
ogomezhe <oscar.gomezherrera@facebook.com>
2016-06-08 10:13:53 -0500

    Initial Commit

---
+++ Prueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba
+//
+//  Created by Oscar on 6/8/16.
+//  Copyright © 2016 Oscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prueba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prueba
+//
+//  Created by Oscar on 6/8/16.
+//  Copyright © 2016 Oscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
